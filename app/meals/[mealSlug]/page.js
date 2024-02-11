31adb6e423698513336342c2c2069b2b80f7da1b
Bob Bass <bob@Bobs-MacBook-Air-M2.local>
2024-02-11 16:45:58 -0600

    finished demo project

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
+// dynamic metadata can be generated with a generateMetadata function that has the same params as the page component
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  // the meal check will run here before the page is rendered
+  if (!meal) notFound();
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) notFound();
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br>");
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        />
+      </main>
+    </>
+  );
+}
